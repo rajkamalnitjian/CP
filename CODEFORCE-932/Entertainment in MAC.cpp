7a2c020e2e4bdf73ecb1fedab745e0b30d311440
rajkamalkumar <rajkamalkumar207@gmail.com>
2024-03-08 16:49:12 +0530

    Q1 done

---
+++ CODEFORCE-932/Entertainment in MAC.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+ void solve(){
+      int n; cin>>n;
+      string s; cin>>s;
+      string temp=s;
+      reverse(temp.begin(),temp.end());
+      cout<<min(s,temp+s)<<endl;
+ }
+int main(){
+    int test;
+     cin>>test;
+     while(test--){
+         solve();
+     }
+    return 0;
+}
+
+//5
+//4
+//cpm
+//2
+//grib
+//10
+//kupitimilablodarbuz
+//1000000000
+//capybara
+//6
+//abacaba
